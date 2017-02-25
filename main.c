22216eb6153f2d581f78f3cd704d77b20504453a
wytrzeszcz <kuba@witomi.pl>
2017-02-25 15:17:34 +0100

    add Trap, and clean a bit

--- main.c
+++ main.c
@@ -16,6 +16,8 @@
 #define TRAP 13 /*T Trap mode for debuging LOL*/
 #define OK 0 
 #define ERRSTACK 100
+typedef enum { false, true } bool;
+
 struct memory //whole machine memory
 {
 	int PC; //Program Counter
@@ -23,10 +25,70 @@ struct memory //whole machine memory
 	int ACC; //Accumulator
 	int CB; //Clipboard
 	int DP; //Data Pointer
+	bool TF; //Trap Flag 
 	char program[256]; //Program Data
 	char data[256]; //Data
 	char stack[256]; //call stack
 }computer_mem;
+char *deassembly(int opcode)
+{
+	static char mnemonic[10];
+	
+	switch(opcode)
+	{
+			case NOP:
+				stpcpy(mnemonic,"NOP_ ");
+				break;
+			case INC:
+				stpcpy(mnemonic,"INC_+");
+				break;				
+			case DEC:
+				stpcpy(mnemonic,"DEC_-");
+				break;
+			case AINC:
+				stpcpy(mnemonic,"AINC_>");
+				break;				
+			case ADEC:
+				stpcpy(mnemonic,"ADEC_<");
+				break;
+			case IN:
+				stpcpy(mnemonic,"IN_.");
+				break;				
+			case OUT:
+				stpcpy(mnemonic,"DEC_,");
+				break;
+			case LOOPS:
+				stpcpy(mnemonic,"LOOPS_[");
+				break;				
+			case LOOPE:
+				stpcpy(mnemonic,"LOOPE_]");
+				break;
+	
+			case COPYD:
+				stpcpy(mnemonic,"COPYD_C");
+				break;				
+			case COPYC:
+				stpcpy(mnemonic,"COPYC_c");
+				break;
+			case PASTED:
+				stpcpy(mnemonic,"PASTED_P");
+				break;				
+			case PASTEC:
+				stpcpy(mnemonic,"PASTEC_p");
+				break;
+			
+			case TRAP:
+				stpcpy(mnemonic,"TRAP_T");
+				break;				
+			default :
+				stpcpy(mnemonic,"ERROR!!!");
+			break;
+			
+			
+	}
+
+	return mnemonic;
+}
 int clear()
 {
 	printf("\nRESETING MEMORY");
@@ -54,91 +116,96 @@ int load()
 	char loaded_char;
 	while((loaded_char=getchar())!=EOF)
 	{
-		if(loaded_char=='+')
+		switch(loaded_char)
 		{
-			computer_mem.program[computer_mem.PC]=INC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='-')
-		{
-			computer_mem.program[computer_mem.PC]=DEC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='>')
-		{
-			computer_mem.program[computer_mem.PC]=AINC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='<')
-		{
-			computer_mem.program[computer_mem.PC]=ADEC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='.')
-		{
-			computer_mem.program[computer_mem.PC]=IN;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char==',')
-		{
-			computer_mem.program[computer_mem.PC]=OUT;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='[')
-		{
-			computer_mem.program[computer_mem.PC]=LOOPS;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char==']')
-		{
-			computer_mem.program[computer_mem.PC]=LOOPE;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='C')
-		{
-			computer_mem.program[computer_mem.PC]=COPYD;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='c')
-		{
-			computer_mem.program[computer_mem.PC]=COPYC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='P')
-		{
-			computer_mem.program[computer_mem.PC]=PASTED;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='p')
-		{
-			computer_mem.program[computer_mem.PC]=PASTEC;
-			computer_mem.PC++;
-			continue;
-		}
-		if(loaded_char=='T')
-		{
-			computer_mem.program[computer_mem.PC]=TRAP;
-			computer_mem.PC++;
-			continue;
+			case '+':
+			
+				computer_mem.program[computer_mem.PC]=INC;
+				computer_mem.PC++;
+				break;
+			
+			case '-':
+			
+				computer_mem.program[computer_mem.PC]=DEC;
+				computer_mem.PC++;
+				break;
+			
+			case '>':
+			
+				computer_mem.program[computer_mem.PC]=AINC;
+				computer_mem.PC++;
+				break;
+			
+			case '<':
+			
+				computer_mem.program[computer_mem.PC]=ADEC;
+				computer_mem.PC++;
+				break;
+			
+			case '.':
+			
+				computer_mem.program[computer_mem.PC]=IN;
+				computer_mem.PC++;
+				break;
+			
+			case ',':
+			
+				computer_mem.program[computer_mem.PC]=OUT;
+				computer_mem.PC++;
+				break;
+			
+			case '[':
+			
+				computer_mem.program[computer_mem.PC]=LOOPS;
+				computer_mem.PC++;
+				break;
+			
+			case ']':
+			
+				computer_mem.program[computer_mem.PC]=LOOPE;
+				computer_mem.PC++;
+				break;
+			
+			case 'C':
+			
+				computer_mem.program[computer_mem.PC]=COPYD;
+				computer_mem.PC++;
+				break;
+			
+			case 'c':
+			
+				computer_mem.program[computer_mem.PC]=COPYC;
+				computer_mem.PC++;
+				break;
+			
+			case 'P':
+			
+				computer_mem.program[computer_mem.PC]=PASTED;
+				computer_mem.PC++;
+				break;
+			
+			case 'p':
+			
+				computer_mem.program[computer_mem.PC]=PASTEC;
+				computer_mem.PC++;
+				break;
+			
+			case 'T':
+			
+				computer_mem.program[computer_mem.PC]=TRAP;
+				computer_mem.PC++;
+				break;
+			default:
+				break;
+			
 		}
 	}
-	
  return OK;
 }
 
 int reset()
 {
+	computer_mem.TF=false;
 	computer_mem.PC=0;
 	return 0;
 }
@@ -150,104 +217,131 @@ int run()
 	{
 		usleep(10*1000);
 		computer_mem.PC%=255;
-		if(computer_mem.program[computer_mem.PC]==NOP)
+		if(computer_mem.TF)
 		{
-			computer_mem.PC++;
-			continue;
-		}
-		if(computer_mem.program[computer_mem.PC]==INC)
-		{
-			computer_mem.ACC=computer_mem.data[computer_mem.DP];
-			computer_mem.ACC++;
-			computer_mem.data[computer_mem.DP]=computer_mem.ACC;
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==DEC)
-		{
-			computer_mem.ACC=computer_mem.data[computer_mem.DP];
-			computer_mem.ACC++;
-			computer_mem.data[computer_mem.DP]=computer_mem.ACC;
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==AINC)
-		{
-			computer_mem.DP++;
-			computer_mem.DP%=256;
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==ADEC)
-		{
-			if(computer_mem.DP==0)computer_mem.DP=255;
-			else computer_mem.DP--;
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==IN)
-		{
-			char in=getchar();
-			computer_mem.data[computer_mem.DP]=in;
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==OUT)
-		{
-			printf("%c",computer_mem.data[computer_mem.DP]);
-			fflush(stdout);
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==LOOPS)
-		{
-			computer_mem.SP++;
-			if(computer_mem.SP>255) return ERRSTACK;
-			computer_mem.PC++;
-			computer_mem.PC%256;
-			computer_mem.stack[computer_mem.SP]=computer_mem.PC;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==LOOPE)
-		{
-			computer_mem.ACC=computer_mem.data[computer_mem.DP];
-			if(computer_mem.ACC==0)
+			printf("REGISTERS:\n");
+			printf("DP:%d\t",computer_mem.DP);
+			printf("PC:%d\t",computer_mem.PC);
+			printf("SP:%d\t",computer_mem.SP);
+			printf("ACC:%d\t",computer_mem.ACC);
+			printf("CB:%c\t",computer_mem.CB);
+			int i=-4;
+			printf("DATA\t|\tCODE\t|\tSTACK\n");
+			for(i;i<5;i++)
 			{
-				if(computer_mem.SP<1) return ERRSTACK;
-				computer_mem.SP--;
-				computer_mem.PC++;
+				printf("%c\t%c\t",computer_mem.data[(computer_mem.DP+i)%256],(i==0)?'<':'|');
+				printf("%s\t%c\t",deassembly(computer_mem.program[(computer_mem.PC+i)%256]),(i==0)?'<':'|');
+				printf("%c\t%c\n",computer_mem.stack[(computer_mem.SP+i)%256],(i==0)?'<':'|');
 			}
-			else 
-			{
-				computer_mem.PC=computer_mem.stack[computer_mem.SP];
-			}
-			continue;
-		}	
-		if(computer_mem.program[computer_mem.PC]==COPYD)
-		{
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==COPYC)
-		{
-			computer_mem.PC++;
-			continue;
-		}		
-		if(computer_mem.program[computer_mem.PC]==PASTED)
-		{
-			computer_mem.PC++;
-			continue;
-		}	
-		if(computer_mem.program[computer_mem.PC]==PASTEC)
-		{
-			computer_mem.PC++;
-			continue;
+			
+			printf("Continue Debuging?(Y/n):");
+			char ans=getchar();
+			if(ans=='n'||ans=='N') computer_mem.TF=false;
 		}
+		switch(computer_mem.program[computer_mem.PC])
+		{
+			case NOP:
 		
-		if(computer_mem.program[computer_mem.PC]==TRAP)
+				computer_mem.PC++;
+				break;
+		
+			case INC:
+			
+				computer_mem.ACC=computer_mem.data[computer_mem.DP];
+				computer_mem.ACC++;
+				computer_mem.data[computer_mem.DP]=computer_mem.ACC;
+				computer_mem.PC++;
+				break;
+				
+			case DEC:
+			
+				computer_mem.ACC=computer_mem.data[computer_mem.DP];
+				computer_mem.ACC++;
+				computer_mem.data[computer_mem.DP]=computer_mem.ACC;
+				computer_mem.PC++;
+				break;
+				
+			case AINC:
+		
+				computer_mem.DP++;
+				computer_mem.DP%=256;
+				computer_mem.PC++;
+				break;
+				
+			case ADEC:
+			
+				if(computer_mem.DP==0)computer_mem.DP=255;
+				else computer_mem.DP--;
+				computer_mem.PC++;
+				break;
+				
+			case IN:
+		
+				char in=getchar();
+				computer_mem.data[computer_mem.DP]=in;
+				computer_mem.PC++;
+				break;
+				
+			case OUT:
+		
+				printf("%c",computer_mem.data[computer_mem.DP]);
+				fflush(stdout);
+				computer_mem.PC++;
+				break;
+				
+			case LOOPS:
+			
+				computer_mem.SP++;
+				if(computer_mem.SP>255) return ERRSTACK;
+				computer_mem.PC++;
+				computer_mem.PC%256;
+				computer_mem.stack[computer_mem.SP]=computer_mem.PC;
+				break;
+					
+			case LOOPE:
+			
+				computer_mem.ACC=computer_mem.data[computer_mem.DP];
+				if(computer_mem.ACC==0)
+				{
+					if(computer_mem.SP<1) return ERRSTACK;
+					computer_mem.SP--;
+					computer_mem.PC++;
+				}
+				else 
+				{
+					computer_mem.PC=computer_mem.stack[computer_mem.SP];
+				}
+				break;
+			
+			case COPYD:
+		
+				computer_mem.CB=computer_mem.data[computer_mem.DP];
+				computer_mem.PC++;
+				break;
+				
+			case COPYC:
+				computer_mem.CB=computer_mem.program[computer_mem.PC];
+				computer_mem.PC++;
+				break;
+					
+			case PASTED:
+			
+				computer_mem.data[computer_mem.DP]=CB;
+				computer_mem.PC++;
+				break;
+			
+			case PASTEC:
+			
+				computer_mem.program[computer_mem.PC]=CB;
+				computer_mem.PC++;
+				break;
+			
+		
+		case TRAP:
 		{
+			TF=true;
 			computer_mem.PC++;
-			continue;
+			break;
 		}
 	}
 }
